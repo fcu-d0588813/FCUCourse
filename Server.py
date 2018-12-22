331ade4fe9022d594488502e71f0403281ea0ef6
zengla <zengla0921@gmail.com>
2018-12-23 02:04:47 +0800

    try connect db

--- Server.py
+++ Server.py
@@ -10,20 +10,40 @@ import pandas as pd
 import psycopg2
 
 #連接資料庫
-conn = psycopg2.connect(database="d3l8u727fkdhuh",
-                        user="uskhmdlztebice",
-                        password="5714697bd569731729daa365947918c513374d064055ec40fd3644ed56963f0f",
-                        host="ec2-107-20-237-78.compute-1.amazonaws.com",
-                        port="5432")
-cursor = conn.cursor()
+db_msg=""
+DATABASE_URL = os.environ['postgres://uskhmdlztebice:5714697bd569731729daa365947918c513374d064055ec40fd3644ed56963f0f@ec2-107-20-237-78.compute-1.amazonaws.com:5432/d3l8u727fkdhuh']
+con = None
+try:
+    # create a new database connection by calling the connect() function
+    con = psycopg2.connect(DATABASE_URL)
 
+    #  create a new cursor
+    cur = conn.cursor()
+    
+    # execute an SQL statement to get the HerokuPostgres database version
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+	db_msg=db_version
+       
+     # close the communication with the HerokuPostgres
+    cur.close()
+except Exception as error:
+    print('Cause: {}'.format(error))
+	db_msg='Cause: {}'.format(error)
+
+
+		
 def get(req):
     p = req['queryResult']['parameters']
     msg = 'Course: ' + p['Course'] + '\nTeacher: ' + p['Teacher'] + '\nAction: ' + p['Action']
 	#cursor.execute("SELECT TName from TEACHER where TName='林明言'")
 	#rows = cur.fetchall()
     print(msg)
-    return msg
+    return db_msg
 
 
 from flask import Flask, request, make_response, jsonify
@@ -44,5 +64,4 @@ def webhook():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    
     
