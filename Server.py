a76f2f61dd0b7778b41999d0770c2c839aed1678
yuru <>
2018-12-22 15:35:11 +0800

    server port

--- Server.py
+++ Server.py
@@ -38,5 +38,7 @@ def webhook():
         
     return make_response(jsonify(res))
 
-if __name__ == '__main__':
-    app.run(port=5000)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
