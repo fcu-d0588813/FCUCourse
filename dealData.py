8a18983c33236cb270b2b62a74048abbf231631a
yuru <>
2018-12-25 20:50:05 +0800

    course csv

--- dealData.py
+++ dealData.py
@@ -7,6 +7,25 @@ Created on Sat Dec 22 19:34:59 2018
 
 import pandas as pd
 
+deptId = {"GE":'通識中心', #通識中心
+          "CE":'工學院', #工學院
+          "CB":'商學院', #商學院
+          "CS":'理學院', #理學院
+          "CH":'人社學院', #人社學院
+          "CI":'資電學院', #資電學院
+          "CD":'建設學院', #建設學院
+          "CF":'金融學院', #金融學院
+          "NM":'國際科技與管理學院', #國際科技與管理學院
+          "AS":'建築專業學院', #建築專業學院
+          "PC":'學分學程', #學分學程
+          "XA":'外語文', #外語文
+          "XC":'通識核心課', #通識核心課
+          "XD":'體育選項課', #體育選項課
+          "XE":'綜合班', #綜合班
+          "XF":'統籌科目', #統籌科目
+          "XH":'軍訓', #軍訓
+         }
+
 data = pd.read_csv('106course.csv')
 teacher = pd.read_csv('teacher.csv')
 teacher = set(list(teacher['teacher']))
@@ -57,3 +76,17 @@ comment = pd.DataFrame(dcard,columns=['rate','cnotice','remark'])
 comment['rate'] = [r[0] for r in comment['rate']]
 
 comment.to_csv('CommentInDB.csv',index=False)
+
+sub = pd.read_csv('sub_name.csv')
+sub = sub['1']
+dept=[]
+for s in sub:
+    d = data[data['cname'].str.lower() == s.lower()]['department']
+    d = d[d.duplicated()==False]
+    if len(d[d.str.contains('X')])>0:
+        d = list(d[d.str.contains('X')])[0]
+    else:
+        d = list(d)[0]
+    dept.append(deptId[d])
+
+
