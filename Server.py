9dbd71e28001619537d3360f879ba366ae2e14eb
zengla <zngla0921@gmail.com>
2019-05-07 17:25:58 +0800

    連接AWS RDS

--- Server.py
+++ Server.py
@@ -9,13 +9,22 @@ import os
 import psycopg2
 import random
 
-#連接資料庫
+#AWS RDS資料庫
+conn = psycopg2.connect(database="fcucourse",
+                        user="fcucourseuser",
+                        password="fcufcu2016",
+                        host="fcucourseinstance.cmbjdacwbwjl.us-east-1.rds.amazonaws.com",
+                        port="5432")
+cursor = conn.cursor()
+'''
+#HEROKU資料庫
 conn = psycopg2.connect(database="d3l8u727fkdhuh",
                         user="uskhmdlztebice",
                         password="5714697bd569731729daa365947918c513374d064055ec40fd3644ed56963f0f",
                         host="ec2-107-20-237-78.compute-1.amazonaws.com",
                         port="5432")
 cursor = conn.cursor()
+'''
 
 def get(req):
     p = req['queryResult']['parameters']
